d0130c60bda7c5adc2299e8880c9c3e9395350b1
seunghyeon.han <seunghyeon.han@navercorp.com>
2021-08-01 11:18:47 +0900

    init

---
+++ src/main/kotlin/com/oppa/oppaspringcloudserver/OppaSpringCloudServerApplication.kt
@@ -0,0 +1,13 @@
+package com.oppa.oppaspringcloudserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+class OppaSpringCloudServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<OppaSpringCloudServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/oppa/oppaspringcloudserver/OppaSpringCloudServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oppa.oppaspringcloudserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OppaSpringCloudServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
